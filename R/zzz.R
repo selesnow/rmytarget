20f70f2906761a43adf10a6ba8cfe359e6bf45b1
Alexey Seleznev <selesnow@mail.ru>
2018-07-30 19:01:39 +0300

    Update zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,6 @@
 .onLoad <- function(libname, pkgname) {
   op <- options()
-  op.rmytarget <- list(rmytarget.url                 = "https://target-sandbox.my.com/",
+  op.rmytarget <- list(rmytarget.url                 = "https://target.my.com/",
                        rmytarget.client_id           = "m1W1ofkghcelGZGk",
                        rmytarget.client_secret       = "YJMLYLFUBIW52e4qW7y39XqZebYWbzNIN8MnMo9BXA1iRhrRvX1sPGfexO4NvT97H1q1tdlegRrHyaCJHMlP1ZtKTwLanBtTvlLQVYoxa1R0GSKcUKG3Lm4eYJFI8mtHga75qn7xE4JStf9Xrwh0AhQFnxX0tQMU19fIbpTsFjwfvvgFEQ1FlmbE67Xksx6n1oz5O5RJsAZMepvgNVIEfm6V0vr2sMdlCh00B6XZdubtqXHAPM",
                        rmytarget.default_auth_schema = "Authorization Code Grant")
