aaaba7108529f527f995c865cd9ef92481027e97
Alexey Seleznev <selesnow@gmail.com>
2020-06-11 13:34:31 +0300

    Update myTarGetClientList.R

--- R/myTarGetClientList.R
+++ R/myTarGetClientList.R
@@ -20,7 +20,7 @@ myTarGetClientList <-
     answer <- content(asw, "parsed", "application/json")
     
     # check limit
-    if ( ! myTarCheckLimits(asw) ) stop("Limit error")
+    if ( ! myTarCheckLimits(answer) ) stop("Limit error")
     
     for ( i in answer$items ) {
       temp <- list( id = i$user$id,
